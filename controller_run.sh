69ad923bda04f1ff9f7f56f53ec51e3465c9ef1e
Brian Coventry <bcov@digs.baker>
2018-02-02 12:54:15 -0800

    Added date to id

--- controller_run.sh
+++ controller_run.sh
@@ -3,4 +3,4 @@
 #SBATCH -p long
 #SBATCH -n 1
 #SBATCH -N 1
-stdbuf -oL -eL /home/bcov/sc/pipeline/protein_pipeline/controller.py $SLURM_JOB_ID 1 > controller_$SLURM_JOB_ID.log 2>&1
+for i in $(seq 1 1000); do stdbuf -oL -eL /home/bcov/sc/pipeline/protein_pipeline/controller.py $SLURM_JOB_ID 1 >> controller_$SLURM_JOB_ID.log 2>&1; done
